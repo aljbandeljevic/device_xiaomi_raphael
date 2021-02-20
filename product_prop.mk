fcbf70988a54a087d8af080aa60659acb09cab3b
Joey Huab <joey@evolution-x.org>
2021-02-20 11:55:07 +0900

    raphael: Tree cleanup and updates from MIUI 21.1.26
    * Add missing vendor permissions
    * Address missing manifest entries
    * Disable Adaptive connectivity toggle in Settings app
    * Disable Smart Pixels overlay
    * Unify vendor_overlay extraction
    * Update and cleanup duplicate configs
    * Update audio configs
    * Update compatibility_matrix
    * Update init.mi_thermald.rc
    * Update init.qcom.usb.sh
    * Update props
    
    Revert "raphael: tune ime padding"
    
    This reverts commit 7e3d18b261937dfbeb03de1d9dc518b407d5e7eb.
    
    Revert "raphael: overlay: enable SmartPixels"
    
    This reverts commit f185bb946fe27930afa4ed40cc7f336688eb4a75.
    
    Revert "raphael: Enable fluence voice recording"
    
    This reverts commit 1109fd06ca9e987ac3359909e93dafab4202ed23.
    
    Revert "raphael: overlay: Disable global mode and CDMA choices"
    
    This reverts commit d77f8120ca6ca342c666c04f046d11743a88cc27.

--- product_prop.mk
+++ product_prop.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2020-2021 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,9 +14,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=0
 endif
 
+# Enable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
+
+# Device
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.characteristics=nosdcard
+
 # Dex2oat
 PRODUCT_PRODUCT_PROPERTIES += \
-     dalvik.vm.dex2oat64.enabled=true
+    dalvik.vm.dex2oat64.enabled=true
 
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -23,8 +33,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.stagefright.omx_default_rank.sw-audio=16 \
-    persist.vendor.audio.fluence.voicerec=true
+    debug.stagefright.omx_default_rank.sw-audio=16
 
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
